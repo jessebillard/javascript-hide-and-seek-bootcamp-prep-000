730fe9b829aeb45b80a2610f81b6805350a606e0
Jesse Billard <jessebillard@mac.com>
2018-02-16 16:24:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function increaseRankBy(n) {
   const rankedLists = document.querySelectorAll('.ranked-list li');
   
   for (let i = 0; i < rankedLists.length; i++) {
-     rankedLists[i].innerHTML = parseInt((i + n), 10)
+     rankedLists[i].innerHTML = parseInt((i + n), 10).toString()
   } return rankedLists;
 }
 
